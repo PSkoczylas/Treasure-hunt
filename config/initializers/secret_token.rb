07750a6b6dd4f7360bb485afb877d7640136c5cf
PSkoczylas <piotr.skoczylas93@gmail.com>
2015-09-21 21:52:15 +0200

    Initalize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TreasureHunt::Application.config.secret_key_base = 'dcfc16fff299d0284a002ffd526141e12befdaa05d50df89c4780cd821e2443168e7757020c7b442955399e4576137a1e94bdb57fc2c98b28205a7fcf8eaf21c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TreasureHunt::Application.config.session_store :cookie_store, key: '_treasure_hunt_session'
